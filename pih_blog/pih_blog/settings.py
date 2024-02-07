bef1c1995b58c1663d95c5ff7be55e99269daca5
Xcept1on <129622595+bhorsujal@users.noreply.github.com>
2024-02-07 15:24:26 +0530

    Update settings.py

--- pih_blog/pih_blog/settings.py
+++ pih_blog/pih_blog/settings.py
@@ -150,5 +150,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'avadg252@gmail.com'
-EMAIL_HOST_PASSWORD = 'dheyuvkqahsjpdpd'
+EMAIL_HOST_USER = 'overflowcampus@gmail.com'
+EMAIL_HOST_PASSWORD = 'jywmwjmcuclrjxxo'
